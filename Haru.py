303da13c07b114878c8183d71350cc6e1326e2e4
haroldavis <haroldavis91@gmail.com>
2020-06-10 21:09:23 -0500

    second commit

---
+++ Haru.py
@@ -0,0 +1,14 @@
+user_1 = ''
+user_2 = ''
+age_1 = 0
+age_2 = 0
+
+
+while user_1 = False
+  user_1 = input('enter your name: ')
+  if user_1 == True:
+    print('hi ' + user_1)
+    break
+  else:
+    print('enter a name')  
+
